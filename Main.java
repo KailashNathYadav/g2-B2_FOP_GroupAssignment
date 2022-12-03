7d3534400ce2f069f05768daf212c017ff345301
Kailash Nath Yadav <107490624+KailashNathYadav@users.noreply.github.com>
2022-12-03 15:31:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package g2B2FOPGroupAssignment.main;
+import g2B2FOPGroupAssignment.department.*;
+
+class Main {
+	public static void main(String[] args) {
+		//Create Object form AdminDepartment, HrDepartment and TechDepartment.
+		
+		AdminDepartment adminDept = new AdminDepartment();
+		System.out.println("Welcome to " + adminDept.departmentName());
+		System.out.println(adminDept.getTodaysWork());
+		System.out.println(adminDept.getWorkDeadline());
+		System.out.println(adminDept.isTodayAHoliday());
+		System.out.println();
+		HrDepartment hrDept = new HrDepartment();
+		System.out.println("Welcome to " + hrDept.departmentName());
+		System.out.println(hrDept.doActivity());
+		System.out.println(hrDept.getTodaysWork());
+		System.out.println(hrDept.getWorkDeadline());
+		System.out.println(hrDept.isTodayAHoliday());
+		System.out.println();
+		TechDepartment techDept = new TechDepartment();
+		System.out.println("Welcome to " + techDept.departmentName());
+		System.out.println(techDept.getTodaysWork());
+		System.out.println(techDept.getWorkDeadline());
+		System.out.println(techDept.getTechStackInformation());
+		System.out.println(techDept.isTodayAHoliday());
+	}
+}
